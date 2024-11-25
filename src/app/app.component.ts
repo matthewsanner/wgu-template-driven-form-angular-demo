0ab5ad4cfb4be70652f2ebcd0a95ef5984e60f07
Matthew Sanner <matthewsannerdev@gmail.com>
2024-11-25 17:56:49 -0600

    updates form template, component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'wgu-template-driven-form-angular-demo';
+  title = 'Template Driven Form';
+  @ViewChild('f', { static: false })
+  signupForm!: NgForm;
+  defaultQuestion = 'street';
+  answer = '';
+  honorifics = ['Dr', 'Mr.', 'Mrs.', 'Ms.'];
+  user = {
+    username: '',
+    email: '',
+    securityQuestion: '',
+    answer: '',
+    honorific: '',
+  };
+  submitted = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.securityQuestion = this.signupForm.value.security;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.honorific = this.signupForm.value.honorific;
+    this.signupForm.reset();
+  }
 }
